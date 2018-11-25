ae57b83df2f28277ddfed712c328981196967c90
vlad <vlad.pocheptsov@gmail.com>
2018-11-25 22:46:08 +0400

    Updates

---
+++ precache-manifest.1cd4fd21c30a95fac8ae19a454f1b2c6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dc23aaeb61ebd48e09d",
+    "url": "/exwidget/static/css/main.f540ac2f.chunk.css"
+  },
+  {
+    "revision": "4dc23aaeb61ebd48e09d",
+    "url": "/exwidget/static/js/main.4dc23aae.chunk.js"
+  },
+  {
+    "revision": "4afb42ddad7966619aad",
+    "url": "/exwidget/static/js/1.4afb42dd.chunk.js"
+  },
+  {
+    "revision": "dadc9eb1267dfe502f4f",
+    "url": "/exwidget/static/js/runtime~main.dadc9eb1.js"
+  },
+  {
+    "revision": "7c11c587395da0e8a086a54ed9538c59",
+    "url": "/exwidget/static/media/subtlePrism.7c11c587.svg"
+  },
+  {
+    "revision": "8aa75ecb889b8d20cd8f86fb67510073",
+    "url": "/exwidget/static/media/astronaut.8aa75ecb.svg"
+  },
+  {
+    "revision": "1d75ea1dfdcb40adbab57a561b970679",
+    "url": "/exwidget/static/media/FuturaBook.1d75ea1d.woff"
+  },
+  {
+    "revision": "4fa444221635632b733c714f3550e4cc",
+    "url": "/exwidget/static/media/FuturaBook.4fa44422.woff2"
+  },
+  {
+    "revision": "38e1ed9a9ee49d755d20226b0d66588c",
+    "url": "/exwidget/static/media/Robotolight.38e1ed9a.woff2"
+  },
+  {
+    "revision": "69eee0b43d46be3ad91ef75a76ea3959",
+    "url": "/exwidget/static/media/Roboto.69eee0b4.woff2"
+  },
+  {
+    "revision": "611ac98371c2d77c377041e1a0fd87dc",
+    "url": "/exwidget/static/media/Robotolight.611ac983.woff"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/exwidget/static/media/Roboto.58bf8e0b.woff"
+  },
+  {
+    "revision": "bb8d5323186a0e25e4aef2276d037194",
+    "url": "/exwidget/index.html"
+  }
+];
